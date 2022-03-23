fcdc81a6206a25960e415be98eb2f3d7e42be5a9
Hayley Stewart <hello@hayley.codes>
2022-03-23 09:54:55 +0800

    Added response codes for updating project.

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -112,3 +112,11 @@ class ProjectDetail(APIView):
         )
         if serializer.is_valid():
             serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_204_NO_CONTENT
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
